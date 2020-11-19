bba625fb0cd31f85036b28bef9b37df9e5badc2a
Franklin von Moon <Franklin.m.moon@gmail.com>
2020-11-19 19:50:59 +1100

    Front End UI Drafted

--- Plexensus_webapp.py
+++ Plexensus_webapp.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="192.168.0.214")
---
+++ config.py
@@ -0,0 +1,8 @@
+config = {
+    "Movie Directory": "D:/Movies",
+    "Host": "192.168.0.214",
+    "API": "",
+    "API Xheader": "",
+    "API Yheader": "",
+    "API Call limit": 1000
+}
