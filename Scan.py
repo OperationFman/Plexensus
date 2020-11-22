787d1967792f8579c67656217b271340f288caa7
Franklin von Moon <Franklin.m.moon@gmail.com>
2020-11-22 12:01:17 +1100

    Fixed security issue

--- Scan.py
+++ Scan.py
@@ -1,13 +1,11 @@
 import requests
+from Plexensus_config import *
 
 def apirequest(movie: str) -> dict:
     """Using RapidAPI sends a query string and returns the movies dict with relevant info"""
     url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
     querystring = {"s":movie,"page":"1","r":"json"}
-    headers = {
-        'x-rapidapi-key': "f1b94bda20mshfc448b87ed832f1p1a9785jsn76236c066d8d",
-        'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
-        }
+    headers = headerrequest()
     response = requests.request("GET", url, headers=headers, params=querystring)
     return response.text
 
--- static/MVC.js
+++ static/MVC.js
@@ -1,12 +1,13 @@
 var view = {
     changePoster: function() {
         return document.getElementById("movieposter").src = "https://m.media-amazon.com/images/M/MV5BY2JiYTNmZTctYTQ1OC00YjU4LWEwMjYtZjkwY2Y5MDI0OTU3XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_SX300.jpg";
+        //Remember, Youre pulling data from a database, NOT the API
     },
     changeYear: function() {
-        return document.getElementById("movieyear").innerHTML = "Hello There";
+        return document.getElementById("movieyear").innerHTML = "2018";
     },
     changeName: function() {
-        return document.getElementById("moviename").innerHTML = "General Kenobi!";
+        return document.getElementById("moviename").innerHTML = "Ready Player One";
     }
 };
 
