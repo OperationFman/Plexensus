1f063a0664e828f9b8b37418efc678b413baf064
Franklin von Moon <Franklin.m.moon@gmail.com>
2020-11-23 14:13:59 +1100

    DB built and insertable

---
+++ Plexensus_logic.py
@@ -0,0 +1,34 @@
+#Temp Config
+your_rapidapi_key = "f1b94bda20mshfc448b87ed832f1p1a9785jsn76236c066d8d"
+DBusername = 'plexuser'
+DBpassword = 'plexpass'
+
+import mysql.connector
+
+def headerrequest() -> dict:
+    headers = {
+        'x-rapidapi-key': your_rapidapi_key,
+        'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
+        }
+    return headers
+
+dbconfig = { 'host': '127.0.0.1',
+             'user': DBusername,
+             'password': DBpassword,
+             'database': 'localmoviesdb', }
+
+class UseDatabase:
+
+    def __init__(self, config: dict) -> None:
+        self.configuration = config
+
+    def __enter__(self) -> 'cursor':
+        self.conn = mysql.connector.connect(**self.configuration)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, exc_trace) -> None:
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+    
--- Scan.py
+++ Scan.py
@@ -1,12 +1,22 @@
 import requests
-from Plexensus_config import *
+from Plexensus_logic import *
 
-def apirequest(movie: str) -> dict:
+def api_request(movie: str) -> dict:
     """Using RapidAPI sends a query string and returns the movies dict with relevant info"""
     url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
     querystring = {"s":movie,"page":"1","r":"json"}
     headers = headerrequest()
     response = requests.request("GET", url, headers=headers, params=querystring)
     return response.text
+#print(api_request("Avengers: Endgame"))
 
-print(apirequest("Avengers: Endgame"))
+
+
+def insert_movie(addname: str, addyear: int, addposter: str, nomatchresult: int) -> None:
+    with UseDatabase(dbconfig) as cursor:
+        _SQL = """insert into moviedata
+                (name, year, poster, moviematch, nomatch)
+                values
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (addname, addyear, addposter, 0, nomatchresult))
+#insert_movie('Ready Player One', 2018, 'https://m.media-amazon.com/images/M/MV5BY2JiYTNmZTctYTQ1OC00YjU4LWEwMjYtZjkwY2Y5MDI0OTU3XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_UX182_CR0,0,182,268_AL_.jpg', 0)
